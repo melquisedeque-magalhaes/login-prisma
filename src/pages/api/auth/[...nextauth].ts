0b49e7f63725f26596783755a61bf050eab076eb
Melqui <melqui.sodre15@gmail.com>
2022-03-21 23:45:32 -0300

    login with google and facebook

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import FacebookProvider from "next-auth/providers/facebook";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -8,6 +10,14 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+    })
     // ...add more providers here
   ],
 })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import {
-  Button, Flex, FormControl, FormLabel, IconButton, Input, Stack, Text
+  Button, Flex, FormControl, FormLabel, Input, Stack, Text
 } from '@chakra-ui/react'
 import { GetServerSideProps } from 'next'
 import { getSession, signIn } from 'next-auth/react'
-import { FaGithub } from 'react-icons/fa'
+import { FaFacebook, FaGithub } from 'react-icons/fa'
+import { FcGoogle } from 'react-icons/fc'
 
 export default function SignIn() {
 
@@ -11,9 +12,17 @@ export default function SignIn() {
     await signIn('github')
   }
 
+  async function handleGoogleLogin(){
+    await signIn('google')
+  }
+
+  async function handleFacebookLogin(){
+    await signIn('facebook')
+  }
+
   return (
-    <Flex h="100vh" w="100vw" align="center" justify="center" background="#ccc">
-      <FormControl borderRadius="10px" p="24px" w="100%" maxW="400px" background="#fff" flexDirection="column" justifyContent="space-between">
+    <Flex h="100vh" w="100vw" align="center" justify="center" background="#fff">
+      <FormControl borderRadius="10px" p="24px" w="100%" maxW="400px" background="#F5F5F5" flexDirection="column" justifyContent="space-between">
         
         <Text fontWeight="bold" fontSize="32px" as="h1" color="#7159c1" mb="8">Login</Text>
         
@@ -28,7 +37,9 @@ export default function SignIn() {
         <Button type='submit' mt="8" background="#7159c1" color="#fff" w="100%" colorScheme="purple">Entrar</Button>
 
         <Stack spacing="4" mt="4">
-          <IconButton onClick={handleGithubLogin} aria-label='github' icon={<FaGithub size={24} />} color="#fff" background="#000" colorScheme="" />
+          <Button onClick={handleGithubLogin} aria-label='github' leftIcon={<FaGithub size={24} />} color="#fff" background="#000" colorScheme="">Login com Github</Button>
+          <Button onClick={handleGoogleLogin} aria-label='google' leftIcon={<FcGoogle size={24} />} color="rgba(0, 0, 0, 0.54);" background="#fff" boxShadow="0px 38.4869px 71.4756px rgba(0, 0, 0, 0.07)" colorScheme="">Continue com o Google</Button>
+          <Button onClick={handleFacebookLogin} aria-label='facebook' leftIcon={<FaFacebook size={24} />} color="#fff" background="#1877F2"  colorScheme="">Continue com o Facebook</Button>
         </Stack>
       </FormControl>
     </Flex>
